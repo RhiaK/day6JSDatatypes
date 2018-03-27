f62c7101a5f2df0c7e5c1173333f18b53d7ca33e
RhiaKe <rhiakaulitzke@gmail.com>
2018-03-26 20:09:52 -0600

    3rd commit - performed operations o the crazy object

--- answers.js
+++ answers.js
@@ -5,6 +5,8 @@
 //4. Array
 //5. Object
 //6. Object
+
+//Take it Easy
 /*const rainbow = ["red", "orange", "yellow", "green", "blue", "purple"];
 console.log(rainbow[4]);
 const Rhiannon = {
@@ -16,3 +18,53 @@ const Rhiannon = {
 
 console.log(Rhiannon.hobby);*/
  
+//Crazy Object!
+
+const crazyObject = {
+  taco: [{meat: 'steak',
+         cheese: ['panela', 'queso', 'chihuahua']},
+         {meat: 'chicken',
+          salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]},
+        ],
+  larry: {
+    nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+    quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+    characters: [{
+      name: "Jeff",
+      occupation: "manager"
+    },
+    {
+      name: "funkhauser",
+      occupation: "tv dude"
+    },
+    {
+      name: "susie",
+      occupation: "jeffs wife",
+      favourtieHobby: "Swearing at Larry and Jeff"
+    },
+    ]
+  }
+}
+//1.
+//console.log(crazyObject.taco[1].salsa[5]);
+//2.
+//console.log(crazyObject.larry.quotes[0]);
+//3.
+//console.log(crazyObject.larry.characters[2].favourtieHobby);
+//4.
+//console.log(crazyObject.larry.nicknames[1]);
+//5.
+//console.log(crazyObject.larry.characters[1]);
+
+
+
+
+
+
+
+
+
+
+
+
+
