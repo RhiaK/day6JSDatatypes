b7ae7d18c660362c4db1d659b5b7b4ea5ee7277e
RhiaKe <rhiakaulitzke@gmail.com>
2018-03-26 19:50:21 -0600

    2nd commit - created some data structures and accessed some data

--- answers.js
+++ answers.js
@@ -5,3 +5,14 @@
 //4. Array
 //5. Object
 //6. Object
+/*const rainbow = ["red", "orange", "yellow", "green", "blue", "purple"];
+console.log(rainbow[4]);
+const Rhiannon = {
+	favFood: "bananas",
+	hobby: "reading",
+	homeTown: "Colorado Springs",
+	favDatatype: "objects"
+};
+
+console.log(Rhiannon.hobby);*/
+ 
